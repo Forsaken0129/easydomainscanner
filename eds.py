b3cdd1784dfeddec8263c7a188d142a3cd337907
Forsaken <37268525+Forsaken0129@users.noreply.github.com>
2022-07-05 16:33:32 +0530

    Update eds.py

--- eds.py
+++ eds.py
@@ -1,221 +1,41 @@
-from pprint import pprint
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urljoin
-from status_check import make_requests
-from error_handling import error_handler
-from collections import Counter
+#!/usr/bin/env python3
+#https://github.com/Forsaken0129/easydomainscanner
 import os
-import requests
-import time
-import re
-import sys
-import pathlib
-import asyncio
 
-def main(url , domain):
+def main():
+  print("""
+  #######                             ######                                           #####                                                  
+ #          ##     ####   #   #      #     #   ####   #    #    ##    #  #    #      #     #   ####     ##    #    #  #    #  ######  #####  
+ #         #  #   #        # #       #     #  #    #  ##  ##   #  #   #  ##   #      #        #    #   #  #   ##   #  ##   #  #       #    # 
+ #####    #    #   ####     #        #     #  #    #  # ## #  #    #  #  # #  #       #####   #       #    #  # #  #  # #  #  #####   #    # 
+ #        ######       #    #        #     #  #    #  #    #  ######  #  #  # #            #  #       ######  #  # #  #  # #  #       #####  
+ #        #    #  #    #    #        #     #  #    #  #    #  #    #  #  #   ##      #     #  #    #  #    #  #   ##  #   ##  #       #   #  
+ #######  #    #   ####     #        ######    ####   #    #  #    #  #  #    #       #####    ####   #    #  #    #  #    #  ######  #    # 
+                                                                                                                                                               
+""")
 
-    # a small try and except block for error handling
+def banner():
+	    
+	print ("""
+\033[1;36m[\033[1;39m1\033[1;36m]\033[1;39m  Easy Domain Scanner
+""")
 
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-        exit(0)
-
-    # filtering out the domain/subdomains using my ugly regex skills 
-
-    pattern = r',.*.'+domain+'",' # an example of ugly regex 
-    raw_domains = re.findall(pattern,r.text) 
-    temp_domains = []
-    for i in raw_domains:
-        temp_domains.append(i[1:-2])
-
-    # using Counter for removing the duplicate entries of domains if any.
-   
-    cnt = Counter(temp_domains)
-    print("\u001b[32m[!] Total No of unique domains/subdomains found : " + str(len(cnt)) + "\n\u001b[0m")
-    urls = [] 
-    
-    # for storing https and http urls
-    print("\u001b[34;1m")
-    for i,j in cnt.items():
-        print(i)
-        urls.append("https://"+i) #appending https
-        urls.append("http://"+i)  #appending http
-    print("\n\n")
-    print("\u001b[0m")
-
-    '''
-     if file already exists empty the file , it happens when you run the script againts same domain
-     multiple times 
-    '''
-    with open(domain+'-200.txt', 'w') as empty:
-        empty.write('')
-    with open(domain+'-other.txt', 'w') as empty:
-        empty.write('')
-    with open(domain+'.txt', 'w') as empty:
-        empty.write('')
-    for i in urls:
-            with open(domain+'.txt', 'a') as f:
-                f.write(i+"\n")
-    
-    # if no subdomains found , then exit the program and delete the empty files
-    if len(cnt)==0:
-        os.remove(domain+'.txt')
-        os.remove(domain+'-other.txt')
-        os.remove(domain+'-200.txt')
-        sys.exit()
-   
+def opt_ini():
+	
+    opt=input("\033[1;39mOption : \033[1;39m")
+    os.chdir('module')
     
     
-
-if __name__ == "__main__":
-    if os.name =='nt':
-        os.system("cls") #clear screen
-    num_lines = 0  
-
-    # banner below  
-    banner = """
-    \u001b[35;1m
- 
-              
-██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░██░░░░░░░░████░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░█░░░░░░██████████░░░░░░█
-█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀░░██░░▄▀▄▀░░████░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█
-█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░░░▄▀░░██░░▄▀░░░░████░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░░░▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█
-█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░███████████░░▄▀▄▀░░▄▀▄▀░░██████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░░░░░▄▀░░██░░▄▀░░█
-█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░███░░░░▄▀▄▀▄▀░░░░██████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░█
-█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█████░░░░▄▀░░░░████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░█
-█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░░░░░░░░░▄▀░░███████░░▄▀░░██████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░░░░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░██░░▄▀░░█
-█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█████████░░▄▀░░███████░░▄▀░░██████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███░░▄▀░░██░░▄▀░░░░░░▄▀░░█
-█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░░░░░░░░░▄▀░░███████░░▄▀░░██████████░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██░░▄▀░░█░░░░▄▀░░░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█
-█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░███████░░▄▀░░██████████░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█
-█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░███████░░░░░░██████████░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░██████████░░░░░░█
-██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███
-█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███
-█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███
-█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░████░░▄▀░░███
-█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███
-█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███
-█░░░░░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░░░███
-█████████░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░█████
-█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░░░░░█
-█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█
-█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░██████████░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█
-██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-            """
- 
-    print(banner)
-
-    # checks if the supplied command argument is valid
-    if len(sys.argv)!=2:
-        print("\u001b[31;1m[!] Usage : To Run My tool just simple run This-------->{python3 eshan.py domain}\u001b[0m")
-        sys.exit(1) 
-    
-    domain = sys.argv[1]
-    assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
-    url = "https://dns.bufferover.run/dns?q=." + domain
-
-    # a request is made to the host , that will check for errors (error handling)
-    error_handler(url) #function imported from error_handling.py 
-    main(url , domain) 
-    print("\u001b[0m")
-    print("\u001b[32;1m[!] Want to filter out the alive domains ? (Y/N)")
-    ch = input()
-    if ch.lower() == 'y' or ch.lower() == 'yes':
-        here = pathlib.Path(__file__).parent
-        with open(here.joinpath(domain+'.txt')) as infile:
-            urls = set(map(str.strip, infile))
-        #calling make_requests function for making asynchronous requests 
-        asyncio.run(make_requests( domain ,urls=urls)) # function imported from status_check.py
-        print("\u001b[0m")
-        
-###########################################################################################################################################################################################
-print("IF You want to test XSS vulnerability Type whole URL of the Target Like this--->{https://url}")
-def get_all_forms(url):
-    """Given a `url`, it returns all forms from the HTML content"""
-    soup = bs(requests.get(url).content, "html.parser")
-    return soup.find_all("form")
-
-
-def get_form_details(form):
-    """
-    This function extracts all possible useful information about an HTML `form`
-    """
-    details = {}
-    # get the form action (target url)
-    action = form.attrs.get("action").lower()
-    # get the form method (POST, GET, etc.)
-    method = form.attrs.get("method", "get").lower()
-    # get all the input details such as type and name
-    inputs = []
-    for input_tag in form.find_all("input"):
-        input_type = input_tag.attrs.get("type", "text")
-        input_name = input_tag.attrs.get("name")
-        inputs.append({"type": input_type, "name": input_name})
-    # put everything to the resulting dictionary
-    details["action"] = action
-    details["method"] = method
-    details["inputs"] = inputs
-    return details
-
-
-
-
-
-def submit_form(form_details, url, value):
-    """
-    Submits a form given in `form_details`
-    Params:
-        form_details (list): a dictionary that contain form information
-        url (str): the original URL that contain that form
-        value (str): this will be replaced to all text and search inputs
-    Returns the HTTP Response after form submission
-    """
-    # construct the full URL (if the url provided in action is relative)
-    target_url = urljoin(url, form_details["action"])
-    # get the inputs
-    inputs = form_details["inputs"]
-    data = {}
-    for input in inputs:
-        # replace all text and search values with `value`
-        if input["type"] == "text" or input["type"] == "search":
-            input["value"] = value
-        input_name = input.get("name")
-        input_value = input.get("value")
-        if input_name and input_value:
-            # if input name and value are not None, 
-            # then add them to the data of form submission
-            data[input_name] = input_value
-
-    if form_details["method"] == "post":
-        return requests.post(target_url, data=data)
+    if(opt == '1'):
+	    opt1=input("\n\033[1;39mWebsite For Testing : \033[1;39m")
+	    os.system("python3 1.py "+opt1)
+	    print(" ")	    	    		    	    	    	    
     else:
-        # GET request
-        return requests.get(target_url, params=data)
+        exit()	   
 
-def scan_xss(url):
-    """
-    Given a `url`, it prints all XSS vulnerable forms and 
-    returns True if any is vulnerable, False otherwise
-    """
-    # get all the forms from the URL
-    forms = get_all_forms(url)
-    print(f"[+] Detected {len(forms)} forms on {url}.")
-    js_script = "<Script>alert('hi')</scripT>"
-    # returning value
-    is_vulnerable = False
-    # iterate over all forms
-    for form in forms:
-        form_details = get_form_details(form)
-        content = submit_form(form_details, url, js_script).content.decode()
-        if js_script in content:
-            print(f"[+] XSS Detected on {url}")
-            print(f"[*] Form details:")
-            pprint(form_details)
-            is_vulnerable = True
-            # won't break because we want to print available vulnerable forms
-    return is_vulnerable
+if __name__ == '__main__':
+ main()
+banner()
+opt_ini()
+
+

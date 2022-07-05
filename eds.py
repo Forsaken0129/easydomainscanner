189a329e7b26859b1c59e9db9ac2d2979ce4377c
Forsaken <37268525+Forsaken0129@users.noreply.github.com>
2022-07-05 17:02:41 +0530

    Update eds.py

--- eds.py
+++ eds.py
@@ -18,17 +18,23 @@ def banner():
 	    
 	print ("""
 \033[1;36m[\033[1;39m1\033[1;36m]\033[1;39m  Easy Domain Scanner
+\033[1;36m[\033[1;39m2\033[1;36m]\033[1;39m  Advanced Domain Scanner
+
 """)
 
 def opt_ini():
 	
     opt=input("\033[1;39mOption : \033[1;39m")
-    #os.chdir('easydomainscanner')
+    #os.chdir('')
     
     
     if(opt == '1'):
 	    opt1=input("\n\033[1;39mWebsite For Testing : \033[1;39m")
 	    os.system("python3 1.py "+opt1)
+	    print(" ")
+
+    if(opt == '2'):
+	    os.system("python3 2.py ")
 	    print(" ")	    	    		    	    	    	    
     else:
         exit()	   
@@ -37,5 +43,4 @@ if __name__ == '__main__':
  main()
 banner()
 opt_ini()
-
 
